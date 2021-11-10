833a731dd293c6130d4f0626c25921414898bb3e
M N Mahmud <bussines.mnmahmud@gmail.com>
2021-11-10 13:49:28 +0700

    ubah index dan members kemudian tambah detailmember.php

--- detailMember.php
+++ detailMember.php
@@ -37,12 +37,12 @@
             <div class="sb-sidenav-menu">
                 <div class="nav">
                     <div class="sb-sidenav-menu-heading">Core</div>
-                    <a class="nav-link active" href="index.php">
+                    <a class="nav-link" href="index.php">
                         <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                         Dashboard
                     </a>
                     <div class="sb-sidenav-menu-heading">Interface</div>
-                    <a class="nav-link" href="members.php">
+                    <a class="nav-link active" href="members.php">
                         <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                         Members
                     </a>
@@ -77,7 +77,8 @@
                     <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item">Dashboard</li>
-                            <li class="breadcrumb-item active" aria-current="page">Daftar Anggota</li>
+                            <li class="breadcrumb-item" aria-current="page">Members</li>
+                            <li class="breadcrumb-item active" aria-current="page">Details Member</li>
                         </ol>
                     </nav>
                 </div>
--- members.php
+++ members.php
@@ -90,7 +90,7 @@
                     <h2 class="mt-5 fw-semibold">Dashboard</h2>
                     <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
                         <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="index.php">Dashboard</a></li>
+                            <li class="breadcrumb-item">Dashboard</li>
                             <li class="breadcrumb-item active" aria-current="page">Members</li>
                         </ol>
                     </nav>
@@ -104,7 +104,7 @@
                         <form action="" method="post">
                             <div class="d-flex">
                                 <select name="groups" id="groups" required class="form-select me-3">
-                                    <option value="">All Grroups</option>
+                                    <option value="">All Groups</option>
                                     <option value="">1</option>
                                     <option value="">2</option>
                                 </select>
